9158b3211857ec86b0d8ea6f618b5e0241f17068
itforget CORDEIRO <itforget11@gmail.com>
2023-12-06 20:41:58 +0000

    add button language

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,14 @@ export default function Home() {
       <div className='bg-[#062C26] flex items-center justify-between'>
         <Image className='w-[120px] ml-6' src={Logo} alt='logo' />
         <div className='flex items-center gap-[80px] text-lg font-bold text-[#96a7a4] mr-[80px]'>
-          <a className='hover:opacity-50' href='#SOBRE'>SOBRE</a>
-          <a className='hover:opacity-50' href='#PROJETOS'>PROJETOS</a>
-          <a className='border-[#6655a1] border-2 rounded-xl p-1 text-[#6655a1] hover:opacity-50' href='mailto:italcordeiro.s@gmail.com'>
+          <a className='hover:opacity-50 border-[#96a7a4] border-2 rounded-xl p-2 text-[#96a7a4]' href='#SOBRE'>SOBRE</a>
+          <a className='hover:opacity-50 border-[#96a7a4] border-2 rounded-xl p-2 text-[#96a7a4]' href='#PROJETOS'>PROJETOS</a>
+          <a className='border-[#6655a1] border-2 rounded-xl p-2 text-[#6655a1] hover:opacity-50' href='mailto:italcordeiro.s@gmail.com'>
             CONTATO
           </a>
         </div>
       </div>
-      <div className={`bg-[#062C26] mx-[250px] my-10 ${styles.arrowContainer}`}>
+      <div className={`bg-[#1A533F] mx-[250px] my-10 p-5 rounded-2xl ${styles.arrowContainer}`}>
         <Image className={`w-[50px] ${styles.arrow}`} src={Arrow} />
         <h1 className='text-[#96a7a4] text-[100px]'>
           Olá <br />
