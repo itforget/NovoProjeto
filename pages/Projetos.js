eb972073f807a23cb75b66c0b23c5bba455d9b80
itforget CORDEIRO <itforget11@gmail.com>
2023-12-11 20:29:42 +0000

    pages

--- pages/Projetos.js
+++ pages/Projetos.js
@@ -13,36 +13,40 @@ export default function Sobre() {
   return (
     <div>
         <Nav toggleTexto={alternarTexto}/>
-        <div className="flex flex-wrap justify-center place-content-center bg-[#1A533F]  my-2 mx-16 p-5 rounded-[120px]">
-        <div className='bg-[#062C26] flex flex-wrap flex-col m-10 p-11 rounded-3xl text-[#96a7a4] font-bold text-xl gap-10 max-w-4xl place-content-center justify-evenly'>
+        <div className="flex flex-wrap justify-center place-content-center bg-[#1A533F]  my-2 mx-16 rounded-[120px]">
+        <div className='bg-[#062C26] flex flex-wrap flex-col m-5 p-11 rounded-3xl text-[#96a7a4] font-bold text-xl gap-10 max-w-4xl place-content-center justify-evenly'>
           <div className='text-5xl my-5 flex justify-center'>
             <h1>PROJETOS</h1>
           </div>
           <div className='flex flex-row gap-6 flex-wrap justify-evenly '>
-          <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
-            <h2 className='flex justify-center mb-2'>PORTIFOLIO</h2>
-            <Image className='w-52 flex justify-center' loading='lazy' src={Portifolio}/>
-          </div>
-          <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
-            <h2 className='flex justify-center mb-2'>CALCULADORA IR</h2>
-            <Image className='w-52 flex justify-center' loading='lazy' src={Calculadora} />
-          </div>
-          <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
-            <h2 className='flex justify-center mb-2'>EM CONSTRUÇÃO</h2>
-            <Image className='w-52 flex justify-center' loading='lazy' src={Place}/>
-          </div>
-          <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
-            <h2 className='flex justify-center mb-2'>EM CONSTRUÇÃO</h2>
-            <Image className='w-52 flex justify-center' loading='lazy' src={Place}/>
-          </div>
-          <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
-            <h2 className='flex justify-center mb-2'>EM CONSTRUÇÃO</h2>
-            <Image className='w-52 flex justify-center' loading='lazy' src={Place}/>
-          </div>
-          <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
-            <h2 className='flex justify-center mb-2'>EM CONSTRUÇÃO</h2>
-            <Image className='w-52 flex justify-center' loading='lazy' src={Place}/>
-          </div>
+            <a href='https://github.com/itforget/PortifolioFinal' target='_blank'>
+            <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl hover:opacity-50 '>
+              <h2 className='flex justify-center mb-2'>PORTIFOLIO</h2>
+              <Image className='w-60 flex justify-center' loading='lazy' src={Portifolio}/>
+            </div>
+            </a>
+            <a href='https://github.com/itforget/CalculadoraDeImposto' target='_blank'>
+            <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl hover:opacity-50'>
+              <h2 className='flex justify-center mb-2'>CALCULADORA IR</h2>
+              <Image className='w-60 flex justify-center' loading='lazy' src={Calculadora} />
+            </div>
+            </a>
+            <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl hover:opacity-50'>
+              <h2 className='flex justify-center mb-2'>EM CONSTRUÇÃO</h2>
+              <Image className='w-60 flex justify-center' loading='lazy' src={Place}/>
+            </div>
+            <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
+              <h2 className='flex justify-center mb-2'>EM CONSTRUÇÃO</h2>
+              <Image className='w-60 flex justify-center' loading='lazy' src={Place}/>
+            </div>
+            <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
+              <h2 className='flex justify-center mb-2'>EM CONSTRUÇÃO</h2>
+              <Image className='w-60 flex justify-center' loading='lazy' src={Place}/>
+            </div>
+            <div className='bg-slate-900 flex flex-col justify-center place-content-center px-5 py-5 rounded-3xl'>
+              <h2 className='flex justify-center mb-2'>EM CONSTRUÇÃO</h2>
+              <Image className='w-60 flex justify-center' loading='lazy' src={Place}/>
+            </div>
           </div>
         </div>
       </div>
